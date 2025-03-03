21f427147f3fdde784d74009a08f794a3c8e7c21
tarek <tarek.moussaoui22007@gmail.com>
2025-03-03 07:54:44 +0100

    commit

--- src/main/java/mtdev/tarek/musicmicroservice/MusicMicroserviceApplication.java
+++ src/main/java/mtdev/tarek/musicmicroservice/MusicMicroserviceApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MusicMicroserviceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MusicMicroserviceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MusicMicroserviceApplication.class, args);
+    }
 
 }
