91051193ac3b4b26516fb77f7378774e2d148223
tarek <tarek.moussaoui22007@gmail.com>
2025-03-03 07:50:45 +0100

    First commit of Music

---
+++ src/main/java/mtdev/tarek/musicmicroservice/MusicMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package mtdev.tarek.musicmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mtdev/tarek/musicmicroservice/MusicMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package mtdev.tarek.musicmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
